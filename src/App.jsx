93bb1ed1d9d0e1770ed86b426356725d978cb244
bouchra91 <you@example.com>
2025-11-16 18:41:06 +0000

    implemanting cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {ScrollTrigger ,SplitText } from 'gsap/all';
 import gsap from 'gsap';
 import NavBar from "./component/NavBar";
 import Hero from "./component/hero";
+import Cocktails from "./component/cocktails";
 gsap.registerPlugin(ScrollTrigger, SplitText); // <- register GSAP plugins it makes sure that the plugins are loaded globally
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <main>
       <NavBar />
       <Hero />
+      <Cocktails />
     </main>
   );
 }
